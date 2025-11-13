aac578dfa121ef27915508666b0181e2fe403b30
Philip Munksgaard <philip@munksgaard.me>
2025-11-13 20:45:14 +0100

    🔥 initial commit 🔥

---
+++ lib/can_dialyzer.ex
@@ -0,0 +1,18 @@
+defmodule CanDialyzer do
+  @moduledoc """
+  Documentation for `CanDialyzer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CanDialyzer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/can_dialyzer_test.exs
@@ -0,0 +1,8 @@
+defmodule CanDialyzerTest do
+  use ExUnit.Case
+  doctest CanDialyzer
+
+  test "greets the world" do
+    assert CanDialyzer.hello() == :world
+  end
+end
